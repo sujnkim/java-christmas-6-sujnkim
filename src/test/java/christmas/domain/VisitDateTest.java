697704c479e5a604ba21a05514b6ef3ecb499eae
sujnkim <blackout3210@naver.com>
2023-11-13 01:37:53 +0900

    test: 방문 날짜 범위를 validate하는 기능 테스트

---
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -0,0 +1,20 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+class VisitDateTest {
+
+    @DisplayName("[EXCEPTION] 날짜 범위가 1부터 31까지에 속하지 않는 경우 예외 발생")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 32})
+    void createVisitDateByInvalidRangeDate(int input) {
+        assertThatThrownBy(() -> new VisitDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+}
