eeed7de3b52fd0e13a2f7054c9157cde9a6fa665
sujnkim <blackout3210@naver.com>
2023-11-13 00:31:22 +0900

    feat: 주문메뉴 총수량이 20개를 넘는 경우를 validate하는 기능 추가

--- src/main/java/christmas/domain/MenuOrder.java
+++ src/main/java/christmas/domain/MenuOrder.java
@@ -3,14 +3,17 @@ package christmas.domain;
 import java.util.EnumMap;
 import java.util.Map;
 
+import static christmas.utils.ErrorMessage.INVALID_MAX_QUANTITY;
 import static christmas.utils.ErrorMessage.INVALID_ORDER;
 
 public class MenuOrder {
+    private final static int MAX_QUANTITY = 20;
 
     private final Map<Menu, Integer> menuOrder;
 
     public MenuOrder(Map<String, Integer> menus) {
-        validateOrder(menus);
+        validateQuantityPerMenu(menus);
+        validateMaxQuantitySum(menus);
         this.menuOrder = createOrder(menus);
     }
 
@@ -25,14 +28,28 @@ public class MenuOrder {
         return menuOrder;
     }
 
-    private void validateOrder(Map<String, Integer> menus){
+    private void validateQuantityPerMenu(Map<String, Integer> menus){
         menus.entrySet().stream()
                 .forEach(menu->validateQuantity(menu.getValue()));
     }
 
     private void validateQuantity(int count){
         if(count < 1){
-            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+            throw new IllegalArgumentException(
+                    INVALID_ORDER.getMessage());
         }
     }
+
+    private void validateMaxQuantitySum(Map<String, Integer> menus){
+        int quantity = menus.entrySet().stream()
+                .map(menu->menu.getValue())
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        if(quantity > MAX_QUANTITY){
+            throw new IllegalArgumentException(
+                    INVALID_MAX_QUANTITY.getMessage());
+        }
+    }
+
 }
