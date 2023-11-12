a7b6a1472f4738ac717b827f14830349ba9851f3
sujnkim <blackout3210@naver.com>
2023-11-13 02:49:25 +0900

    test: 디데이 할인 금액 메서드 테스트 코드

--- src/test/java/christmas/domain/DateDiscountTest.java
+++ src/test/java/christmas/domain/DateDiscountTest.java
@@ -2,10 +2,13 @@ package christmas.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class DateDiscountTest {
@@ -19,5 +22,19 @@ class DateDiscountTest {
                 DateEvent.CHRISTMAS,
                 DateEvent.WEEKDAY,
                 DateEvent.SPECIAL));
+    }
+
+    @DisplayName("[SUCCESS] 해당 날짜에 적용되는 디데이 할인 금액을 반환한다")
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1,1000",
+            "2,1100",
+            "25,3400",
+            "30,0"
+    })
+    void getChristmasDDayDiscountPrice(int input, int expected) {
+        assertThat(
+                DateEvent.getDDayDiscountPrice(input)
+        ).isEqualTo(expected);
     }
 }
