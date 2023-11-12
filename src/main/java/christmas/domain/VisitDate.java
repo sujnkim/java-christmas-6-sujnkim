2024780989050696bc3a359187e1a27bb71587b7
sujnkim <blackout3210@naver.com>
2023-11-13 01:37:30 +0900

    feat: 방문 날짜를 관리하는 VisitDate 클래스 생성

---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+import static christmas.utils.ErrorMessage.INVALID_VISIT_DATE;
+
+public class VisitDate {
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+
+    private int visitDate;
+
+    public VisitDate(int visitDate) {
+        validate(visitDate);
+        this.visitDate = visitDate;
+    }
+
+    private void validate(int visitDate) {
+        if (visitDate < MIN_DATE || visitDate > MAX_DATE) {
+            throw new IllegalArgumentException(INVALID_VISIT_DATE.getMessage());
+        }
+    }
+}
