f1e36ffde9edd59b3747ad9b764757bb30d62fdd
sujnkim <blackout3210@naver.com>
2023-11-13 00:26:54 +0900

    feat: 에러 메시지 추가 - 총수량이 20을 초과하는 경우 예외 발생

--- src/main/java/christmas/utils/ErrorMessage.java
+++ src/main/java/christmas/utils/ErrorMessage.java
@@ -1,7 +1,8 @@
 package christmas.utils;
 
 public enum ErrorMessage {
-    INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    INVALID_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_MAX_QUANTITY("메뉴는 한 번에 총 20개까지만 주문할 수 있습니다. 개수를 조정하여 다시 입력해주세요.");
 
 
 
