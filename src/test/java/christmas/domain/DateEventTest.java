efea3e87375792795af9e560f9f2a1c4911f7d2c
sujnkim <blackout3210@naver.com>
2023-11-14 01:39:44 +0900

    test: DateEvent 할인가 가격 계산 테스트

--- src/test/java/christmas/domain/DateEventTest.java
+++ src/test/java/christmas/domain/DateEventTest.java
@@ -1,16 +1,28 @@
 package christmas.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class DateEventTest {
+    Visitor visitor;
+
+    @BeforeEach
+    void setUp() {
+        VisitDate visitDate = new VisitDate(25);
+        Map<String, Integer> menus = new HashMap<String, Integer>();
+        menus.put("양송이수프", 1);
+        menus.put("티본스테이크", 2);
+        menus.put("초코케이크", 3);
+        visitor = new Visitor(visitDate, new MenuOrder(menus));
+    }
 
     @DisplayName("[SUCCESS] 해당 날짜에 적용되는 이벤트의 리스트를 반환한다")
     @Test
@@ -23,17 +35,25 @@ class DateEventTest {
                 DateEvent.SPECIAL));
     }
 
-    @DisplayName("[SUCCESS] 해당 날짜에 적용되는 디데이 할인 금액을 반환한다")
-    @ParameterizedTest
-    @CsvSource(value = {
-            "1,1000",
-            "2,1100",
-            "25,3400",
-            "30,0"
-    })
-    void getChristmasDDayDiscountPrice(int input, int expected) {
-        assertThat(
-                DateEvent.getDDayDiscountPrice(input)
-        ).isEqualTo(expected);
+    @DisplayName("[SUCCESS]해당 고객에게 적용되는 크리스마스 디데이 할인가를 반환")
+    @Test
+    void calculateChristmasDDAyDiscount() {
+        assertThat(DateEvent.CHRISTMAS.calculateDiscount(visitor))
+                .isEqualTo(3400);
     }
+
+    @DisplayName("[SUCCESS]해당 고객에게 적용되는 주중 할인가를 반환")
+    @Test
+    void calculateWeekDayDiscount() {
+        assertThat(DateEvent.WEEKDAY.calculateDiscount(visitor))
+                .isEqualTo(3 * 2023);
+    }
+
+    @DisplayName("[SUCCESS]해당 고객에게 적용되는 주중 할인가를 반환")
+    @Test
+    void calculateWeekendDiscount() {
+        assertThat(DateEvent.WEEKEND.calculateDiscount(visitor))
+                .isEqualTo(2 * 2023);
+    }
+
 }
