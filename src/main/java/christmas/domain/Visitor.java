810dc8f705ba8568bd112498a29fb02efc9b10eb
sujnkim <blackout3210@naver.com>
2023-11-14 01:12:58 +0900

    feat: 사용자의 입력 정보를 관리하는 Visitor 클래스 추가

---
+++ src/main/java/christmas/domain/Visitor.java
@@ -0,0 +1,25 @@
+package christmas.domain;
+
+import java.util.EnumMap;
+
+public class Visitor {
+    private final VisitDate date;
+    private final MenuOrder menuOrder;
+
+    public Visitor(VisitDate visitDate, MenuOrder menuOrder) {
+        this.date = visitDate;
+        this.menuOrder = menuOrder;
+    }
+
+    public int getVisitDate() {
+        return date.getVisitDate();
+    }
+
+    public EnumMap<Menu, Integer> getMenuOrder() {
+        return menuOrder.getMenuOrder();
+    }
+
+    public int getTotalMenuPrice() {
+        return menuOrder.calculateTotalMenuPrice();
+    }
+}
