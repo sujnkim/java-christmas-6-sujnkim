0899335bc3584e8451a13d724538af074dffe08c
sujnkim <blackout3210@naver.com>
2023-11-14 17:17:59 +0900

    feat: 사용자 입력을 필요한 자료형으로 바꾸기 위한 converter 작성

---
+++ src/test/java/christmas/utils/InputConverterTest.java
@@ -0,0 +1,30 @@
+package christmas.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputConverterTest {
+
+    @DisplayName("[SUCCESS]사용자가 입력한 각 메뉴를 split하여 Map에 담아 반환한다")
+    @Test
+    void getMenuOrderAndSplit() {
+        String value = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+        Map<String, Integer> result = new HashMap<>();
+        result.put("티본스테이크", 1);
+        result.put("바비큐립", 1);
+        result.put("초코케이크", 2);
+        result.put("제로콜라", 1);
+
+        assertThat(
+                InputConverter.getMenuOrder(value)
+        ).isEqualTo(result);
+    }
+
+}
