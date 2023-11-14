78d84b7488b07c42c4cd3c2ff9b07dd61cf61aa4
sujnkim <blackout3210@naver.com>
2023-11-14 22:47:25 +0900

    refactor: Console.close 위치 변경

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,12 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.PlannerMainController;
 
 public class Application {
     public static void main(String[] args) {
         PlannerMainController planner = new PlannerMainController();
         planner.start();
+        Console.close();
     }
 }
